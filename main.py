073345ad606669f3b5d8832f2ffd5fdf3d6f4f7c
masterteerat <120311945+masterteerat@users.noreply.github.com>
2023-06-17 19:19:40 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
-ans = 0
-ascii2 = randint(48, 57)
+score = 0
 #ascii2 = 255
-t = 0
-T = 0
+ms = 0
+sec = 0
+ans = 0
+#ascii2 = randint(48, 57)
+life = 0
 weight = 7
-basic.show_string(String.from_char_code(ascii2))
+#basic.show_string(String.from_char_code(ascii2))
 
 while True:
-
-    if input.button_is_pressed(Button.A):
-        music.play(music.string_playable("C5 C5 B B C5 - - - ", 1500),
-            music.PlaybackMode.IN_BACKGROUND)
-        ans = ans + (2 ** weight)
-        weight += -1
-    if input.button_is_pressed(Button.B):
-        music.play(music.string_playable("E E F F G - - - ", 1500),
-            music.PlaybackMode.IN_BACKGROUND)
-        weight += -1
-    if weight == -1:
-        if ans == ascii2:
-            basic.show_icon(IconNames.YES)
-            basic.show_number(T)
+    ms = 0
+    sec = 0
+    ans = 0    
+    ascii2 = 255
+    basic.show_string(String.from_char_code(ascii2))
+    while True:
+        if input.button_is_pressed(Button.A):
+            music.play(music.string_playable("C5 C5 B B C5 - - - ", 1500),
+                music.PlaybackMode.IN_BACKGROUND)
+            ans = ans + 2 ** weight
+            weight += -1
+        if input.button_is_pressed(Button.B):
+            music.play(music.string_playable("E E F F G - - - ", 1500),
+                music.PlaybackMode.IN_BACKGROUND)
+            weight += -1
+        if weight == -1:
+            if ans == ascii2:
+                basic.show_icon(IconNames.YES)
+                score += 1
+                break
+            if ans != ascii2:
+                basic.show_icon(IconNames.NO)
+                life += -1
+                break
+        if sec >= 6:
+            music.play(music.string_playable("F F - - - - - - ", 500),
+                music.PlaybackMode.IN_BACKGROUND)
+        #time out
+        if sec > 11:
+            basic.show_icon(IconNames.SAD)
             break
-        if ans != ascii2:
-            basic.show_icon(IconNames.NO)
-            break
-    t = t + 300
-    T = t // 1000        
-    basic.pause(300)
-           
+        if life < 3:
+            basic.show_icon(IconNames.SAD)
+            break          
+        ms = ms + 300
+        sec = ms // 1000
+        basic.pause(300)
+    weight = 0    
+    
+    if score == 5:
+        basic.show_icon(IconNames.HAPPY)
+        break    
--- main.ts
+++ main.ts
@@ -1,37 +1,67 @@
-let ans = 0
-let ascii2 = randint(48, 57)
+let ascii2: number;
+let score = 0
 // ascii2 = 255
-let t = 0
-let T = 0
+let ms = 0
+let sec = 0
+let ans = 0
+// ascii2 = randint(48, 57)
 let weight = 7
-basic.showString(String.fromCharCode(ascii2))
+// basic.show_string(String.from_char_code(ascii2))
 while (true) {
-    if (input.buttonIsPressed(Button.A)) {
-        music.play(music.stringPlayable("C5 C5 B B C5 - - - ", 1500), music.PlaybackMode.InBackground)
-        ans = ans + 2 ** weight
-        weight += -1
-    }
-    
-    if (input.buttonIsPressed(Button.B)) {
-        music.play(music.stringPlayable("E E F F G - - - ", 1500), music.PlaybackMode.InBackground)
-        weight += -1
-    }
-    
-    if (weight == -1) {
-        if (ans == ascii2) {
-            basic.showIcon(IconNames.Yes)
-            basic.showNumber(T)
+    ms = 0
+    sec = 0
+    ans = 0
+    ascii2 = 255
+    basic.showString(String.fromCharCode(ascii2))
+    while (true) {
+        if (input.buttonIsPressed(Button.A)) {
+            music.play(music.stringPlayable("C5 C5 B B C5 - - - ", 1500), music.PlaybackMode.InBackground)
+            ans = ans + 2 ** weight
+            weight += -1
+        }
+        
+        if (input.buttonIsPressed(Button.B)) {
+            music.play(music.stringPlayable("E E F F G - - - ", 1500), music.PlaybackMode.InBackground)
+            weight += -1
+        }
+        
+        if (weight == -1) {
+            if (ans == ascii2) {
+                basic.showIcon(IconNames.Yes)
+                score += 1
+                break
+            }
+            
+            if (ans != ascii2) {
+                basic.showIcon(IconNames.No)
+                let life += -1
+                break
+            }
+            
+        }
+        
+        if (sec >= 6) {
+            music.play(music.stringPlayable("F F - - - - - - ", 500), music.PlaybackMode.InBackground)
+        }
+        
+        if (sec > 11) {
+            basic.showIcon(IconNames.Sad)
             break
         }
         
-        if (ans != ascii2) {
-            basic.showIcon(IconNames.No)
+        if (life < 3) {
+            basic.showIcon(IconNames.Sad)
             break
         }
         
+        ms = ms + 300
+        sec = Math.idiv(ms, 1000)
+        basic.pause(300)
+    }
+    weight = 0
+    if (score == 5) {
+        basic.showIcon(IconNames.Happy)
+        break
     }
     
-    t = t + 300
-    T = Math.idiv(t, 1000)
-    basic.pause(300)
 }
